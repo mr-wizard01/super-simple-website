dbeb40bfe92fb4d70fde7274160067657d599e66
mr-wizard01 <115467396+mr-wizard01@users.noreply.github.com>
2023-01-21 01:46:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let  btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+
+    let displayHide_toggle=document.querySelector(".list");
+
+    displayHide_toggle.classList.toggle("toggle")
+})
